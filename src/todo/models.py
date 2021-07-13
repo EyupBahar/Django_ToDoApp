b8b0994df7c7b2fd7a44482e45d2d521ee6456cc
7204-OMER <Eyupbhr24@gmail.com>
2021-07-13 15:45:52 +0300

    add models.py

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todolist(models.Model):
+    todo_number = models.IntegerField()
+    todo_content = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return f"{self.todo_number} {self.todo_content}"
