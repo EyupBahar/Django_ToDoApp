3cc7156472cbc6aad92dcbd97a47c460b2edfec5
7204-OMER <Eyupbhr24@gmail.com>
2021-07-15 14:51:37 +0300

    add views

--- src/todo/views.py
+++ src/todo/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
 
-def home_page(request):
-    return render(request, 'todo/home.html') 
+def home(request):
+    return render(request, 'todo/home.html')
+
+def todolist(request):
+    todos = Todo.object.all()
+    context = {
+        
+    }
