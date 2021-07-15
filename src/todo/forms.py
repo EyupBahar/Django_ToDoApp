b152f79093656a99bde89b10deddf5dddf19df0c
7204-OMER <Eyupbhr24@gmail.com>
2021-07-15 16:35:03 +0300

    add forms.py

--- src/todo/forms.py
+++ src/todo/forms.py
@@ -6,3 +6,9 @@ class TodoAppForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = '__all__'
+
+class TodoUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Todo
+        fields = ("title", "Completed")
