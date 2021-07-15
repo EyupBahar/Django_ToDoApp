7d89a587d29180ecea71eded00f1480d9b670e7d
7204-OMER <Eyupbhr24@gmail.com>
2021-07-15 14:51:13 +0300

    add requirement and models

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Todo(models.Model):
-    todo_number = models.IntegerField()
-    todo_content = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    created_date= models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
 
     def __str__(self):
-        return f"{self.todo_number} {self.todo_content}"
+        return self.title
