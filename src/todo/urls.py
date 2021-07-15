0b2d96953640deed9d1c9671298187a8d90ef6cc
7204-OMER <Eyupbhr24@gmail.com>
2021-07-15 17:10:43 +0300

    add urls updates

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, todolist, todo_create, todo_update
+from .views import home, todolist, todo_create, todo_update, todo_delete
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('list/', todolist, name='todolist'),
-    path('create/', todo_create, name='todo_create'),
-    path('<int:id>/update', todo_update, name='todo_update'), #single bir objeyi id si ile alma
+    path("", home, name="home"),
+    path("list/", todolist, name="todolist"),
+    path("create/", todo_create, name="todo_create"),
+    path("<int:id>/update/", todo_update, name="todo_update"),
+    path("<str:id>/delete/", todo_delete, name="todo_delete"),
 ]
